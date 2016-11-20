0f829da69a013d04454a3df5382951065cd9bc12
Jenny <sacxjenny@live.com>
2016-11-19 20:31:13 -0700

    Added font files, created the home screen

--- app/src/main/java/com/example/jennykuma/peticularme/CreateProfile.java
+++ app/src/main/java/com/example/jennykuma/peticularme/CreateProfile.java
@@ -1,7 +1,9 @@
 package com.example.jennykuma.peticularme;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class CreateProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+        Typeface roboto = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+
+        TextView addProfilePic = (TextView)findViewById(R.id.addProfPic_text);
+        TextView petInfo = (TextView)findViewById(R.id.pet_info);
+
+        addProfilePic.setTypeface(roboto);
+        petInfo.setTypeface(roboto);
     }
 }
