3e6c3e3beb0e95220befc11c4c2db061c61e097d
eunwolie <kyookiyomi@gmail.com>
2016-11-27 00:37:02 -0700

    Bottom Sheet Made

--- app/src/main/java/com/example/jennykuma/peticularme/Profile.java
+++ app/src/main/java/com/example/jennykuma/peticularme/Profile.java
@@ -1,8 +1,11 @@
 package com.example.jennykuma.peticularme;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -27,6 +30,14 @@ public class Profile extends AppCompatActivity {
         gender.setTypeface(roboto);
         breed.setTypeface(roboto);
         description.setTypeface(roboto);
+
+        Button bottomSheet = (Button)findViewById(R.id.share);
+        bottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, BottomSheet.class));
+            }
+        });
 
     }
 }
