b0e73315326a0adcdd3624a59916ac7ec63db3e4
Doxol <david.sepulvedamarti@ucalgary.ca>
2016-11-26 20:16:13 -0700

    Takes pictures and crops them hopefully
    this

--- app/src/main/java/com/example/jennykuma/peticularme/CreateProfile.java
+++ app/src/main/java/com/example/jennykuma/peticularme/CreateProfile.java
@@ -1,16 +1,27 @@
 package com.example.jennykuma.peticularme;
 
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.shapes.Shape;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -25,6 +36,8 @@ import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.view.Menu;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.app.Activity;
 
 import org.w3c.dom.Text;
 
@@ -35,15 +48,21 @@ public class CreateProfile extends AppCompatActivity implements View.OnClickList
     private EditText petBirthday;
     private String[] breeds;
 
+    private TextView addProfPic_text;// = (EditText)findViewById(R.id.addProfPic_text);
+    private Button addProfPic_btn;// = (Button) findViewById(R.id.addProfPic_btn);
+    private View addPic_rect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
 
         Typeface roboto = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
-
-        TextView addProfilePic = (TextView)findViewById(R.id.addProfPic_text);
         TextView petInfo = (TextView)findViewById(R.id.pet_info);
+
+        addProfPic_text = (TextView)findViewById(R.id.addProfPic_text);
+        addProfPic_btn = (Button) findViewById(R.id.addProfPic_btn);
+        addPic_rect = (View) findViewById(R.id.addPicture_rectangle);
 
         EditText petName = (EditText)findViewById(R.id.pet_name);
         EditText petBirthday = (EditText)findViewById(R.id.pet_birthday);
@@ -53,7 +72,7 @@ public class CreateProfile extends AppCompatActivity implements View.OnClickList
         TextView breedLabel = (TextView)findViewById(R.id.breed_label);
         EditText petDescription = (EditText)findViewById(R.id.pet_description);
 
-        addProfilePic.setTypeface(roboto);
+        addProfPic_text.setTypeface(roboto);
         petInfo.setTypeface(roboto);
         petName.setTypeface(roboto);
         petBirthday.setTypeface(roboto);
@@ -62,7 +81,6 @@ public class CreateProfile extends AppCompatActivity implements View.OnClickList
         genderLabel.setTypeface(roboto);
         breedLabel.setTypeface(roboto);
         petDescription.setTypeface(roboto);
-
         getBirthday();
         getBreed();
     }
@@ -96,6 +114,56 @@ public class CreateProfile extends AppCompatActivity implements View.OnClickList
     public void onClick(View view){
         if(view == petBirthday){
             datePickerDialog.show();
+        }
+    }
+    public void onClickBtn(View view){
+        //dispatchTakePictureIntent();
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Profile Picture");
+        alertDialog.setMessage("Take a profile photo or choose from gallery?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Camera",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dispatchTakePictureIntent();
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Gallery            ",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //BitmapDrawable bd = new BitmapDrawable(getResources(), imageBitmap);
+            addProfPic_btn.setVisibility(View.GONE);
+            addProfPic_text.setVisibility(View.GONE);
+            //addPic_rect.setBackground(bd);
+            RoundedBitmapDrawable roundDrawable = RoundedBitmapDrawableFactory.create(getResources(), imageBitmap);
+            roundDrawable.setCircular(true);
+            addPic_rect.setBackground(roundDrawable);
         }
     }
 
